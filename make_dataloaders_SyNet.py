ad344cc4cedaf1fab270e9dafbea9e54ab9582e2
Chih-Fan Chang <raecbce1216@gmail.com>
2023-03-02 13:50:34 +0100

    upload files for VAE_SyNet

---
+++ make_dataloaders_SyNet.py
@@ -0,0 +1,37 @@
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+import pandas as pd
+import numpy as np
+
+
+class SyNet_Dataset(Dataset):
+    def __init__(self, gene_exp_file, transform=None):
+        self.gene_exp_df = pd.read_csv(gene_exp_file, index_col=[0])  # Not sure if index should be specified or not. 
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.gene_exp_df)
+
+    def __getitem__(self, idx):
+        gene_exp = np.array([self.gene_exp_df.iloc[idx, 1:]])
+        if self.transform:
+            gene_exp = self.transform(gene_exp)  # Not sure if we need this transform argument here. 
+        
+        return gene_exp
+
+
+def CSV_reader(train_data_path, test_data_path, batch_size=128, shuffle=True):
+    train_data = SyNet_Dataset(gene_exp_file = train_data_path)
+    test_data = SyNet_Dataset(gene_exp_file = test_data_path)
+
+    train_dataloader = DataLoader(train_data, batch_size, shuffle)
+    test_dataloader = DataLoader(test_data, batch_size, shuffle)
+
+    return train_dataloader, test_dataloader
+
+# df_train_data = pd.read_csv("/hpc/compgen/users/cchang/Projects/gene_exp_VAE/data/df_train_data_gene_exp.csv")
+# df_test_data = pd.read_csv("/hpc/compgen/users/cchang/Projects/gene_exp_VAE/data/df_test_data_gene_exp.csv")
+
+# train_data = torch.from_numpy(df_train_data.to_numpy())
+# test_data = torch.from_numpy(df_test_data.to_numpy())
+
---
+++ run_VAE_SyNet.py
@@ -0,0 +1,169 @@
+# std libs
+import argparse
+
+
+# torch libs
+import torch
+import torch.nn as nn
+import torch.utils.data
+import torch.nn.parallel
+import torch.optim as optim
+
+
+from make_dataloaders_SyNet import CSV_reader
+
+# Key word arguments
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('-e', '--epochs', type=int, default=100, help='epochs')
+parser.add_argument('-b', '--bs', type=str, default=128, help='batch size')
+parser.add_argument('--trd', type=str, help='train data path')
+parser.add_argument('--ted', type=str, help='test data path')
+parser.add_argument('--lr', type=float, default=0.0002, help='learning rate, default=0.0002')
+
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+class Encoder(nn.Module):
+    def __init__(self, encoder_dims = [1000, 500, 200], input_size = None):
+        if encoder_dims == None or input_size == None:
+            raise ValueError('Must explicitly declare input size and latent space dimension')
+            
+        super(Encoder, self).__init__()
+        self.inp_dim = input_size
+        self.zdim = encoder_dims[-1]
+
+        current_dim = input_size
+        self.layers = nn.ModuleList()
+        for hdim in encoder_dims:
+            print("current_hdim: " + str(hdim))
+            if hdim == self.zdim:
+                self.layers.append(nn.Linear(current_dim, 2*hdim))  # 2 because we have std and mean. 
+                self.layers.append(nn.LeakyReLU(0.2))
+                self.layers.append(nn.BatchNorm1d(2*hdim))
+            else:
+                self.layers.append(nn.Linear(current_dim, hdim))
+                self.layers.append(nn.LeakyReLU(0.2))
+                self.layers.append(nn.BatchNorm1d(hdim))
+            current_dim = hdim
+
+    def forward(self, x):        
+            """
+            
+            Forward pass of the encoder
+            
+            """
+
+            out = self.layers(x);
+            # get mean and variance 
+            mu, variance = out.chunk(2, dim=1)      
+            
+            return mu, variance
+    
+class Decoder(nn.Module):
+    def __init__(self, decoder_dims = [200, 500, 1000], input_size = None):
+        """
+            
+        The decoder class
+            
+        """
+        #if latent_dim == None or input_size == None:
+         #   raise ValueError('Must explicitly declare input size and latent space dimension (2*latent_dim for enc)')
+            
+        super(Decoder, self).__init__();
+        
+        if decoder_dims == None or input_size == None:
+            raise ValueError('Must explicitly declare input size (==output size) and decoder layer size')
+            
+        self.inp_dim = input_size
+        #self.zdim = decoder_dims[-1]
+
+        current_dim = input_size
+        self.layers = nn.ModuleList()
+        for hdim in decoder_dims:
+            print("current_hdim: " + str(hdim))
+            self.layers.append(nn.Linear(current_dim, hdim))
+            self.layers.append(nn.LeakyReLU(0.2))
+            self.layers.append(nn.BatchNorm1d(hdim))
+            current_dim = hdim
+        self.layers.append(nn.Linear(current_dim, input_size))
+
+    def forward(self, z):
+        """
+            
+        Forward pass of the Decoder
+            
+        """
+        
+        out =  self.layers(z);
+        return out
+    
+class VAE(nn.Module):
+    def __init__(self, Encoder, Decoder):
+        super(VAE, self).__init__() #initialises OrderedDicts to hold network parameters etc. https://stackoverflow.com/a/61288819 
+        self.Encoder = Encoder  ### Encoder is an attribute
+        self.Decoder = Decoder  ### Attribute created in .__init__() are called instance attributes. 
+
+
+    def loss_function(self, pred, target, mean, log_var, average = False):
+        reconstruction_loss = (pred - target).view(pred.size(0), -1)
+        reconstruction_loss = reconstruction_loss**2
+        reconstruction_loss = torch.sum(reconstruction_loss, dim=-1)
+        if average:
+            reconstruction_loss = reconstruction_loss.mean()
+        else:
+            reconstruction_loss = reconstruction_loss.sum()
+
+        kl_loss = - 0.5 * torch.sum(1+ log_var - mean.pow(2) - log_var.exp())
+
+        return reconstruction_loss + kl_loss
+
+    def reparameterization(self, mean, var):            # mean and log variance from the encoder's latent space
+        epsilon = torch.randn_like(var).to(DEVICE)      # sampling epsilon        
+        z = mean + var*epsilon                          # reparameterization trick  ### WHY is it var not sigma(std)?
+        return z
+        
+    def forward(self, x):
+        mu, log_var = self.Encoder(x)
+        # the latent space mu and variance, but reparametrized
+        z = self.reparameterization(mu, torch.exp(0.5 * log_var))
+        # the reconstructed data
+        x_r = self.Decoder(z)
+            
+        return  mu, z, log_var
+
+def main():
+    global args, model
+    args = parser.parse_args()
+
+    
+    print(DEVICE) 
+
+    train_dataloader, test_dataloader = CSV_reader(train_data_path=args.trd, test_data_path=args.ted, batch_size=args.bs, shuffle=True)
+    inp_size = [batch[0].shape[1] for _, batch in enumerate(train_dataloader, 0)][0]
+
+    model = VAE(Encoder, Decoder).to(DEVICE)  # Or give number of layers as arguments.
+    optimizer = optim.Adam(model.parameters(), lr=args.lr)
+    model.train()
+
+    for epoch in range(args.epochs):
+        overall_loss = 0
+        for batch_idx, x in enumerate(train_dataloader):
+            x = x.view(args.bs, inp_size)  # Returns a new tensor with the same data but of a different shape as given.
+            x = x.to(DEVICE)
+
+            optimizer.zero_grad()
+
+            x_hat, mean, log_var = model(x)
+            loss = VAE.loss_function(x, x_hat, mean, log_var)
+            
+            overall_loss += loss.item()
+            
+            loss.backward()
+            optimizer.step()
+        
+        print("\tEpoch", epoch + 1, "complete!", "\tAverage Loss: ", overall_loss / (batch_idx*args.bs))
+        
+    print("Finish!!")
+    
+
+if __name__ == "__main__":
+    main()  
